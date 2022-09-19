f5e95533894b3e49cd3dfd4e1205eeff5c15a660
Loay Al Rashdi <loay@Loays-MacBook-Pro.local>
2022-09-19 02:11:19 -0500

    Also added the alpha checker

--- src/components/OpeningTester/OpeningTester.tsx
+++ src/components/OpeningTester/OpeningTester.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { moveHistory } from "../../Constants";
 type Props = {
   moveHistory: moveHistory[] | undefined;
@@ -15,10 +15,35 @@ const OpeningTester: React.FC<Props> = ({
   const [testingOpening, setTestingOpening] = useState<
     moveHistory[] | undefined
   >();
+  const [testingMode, setTestingMode] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (testingMode && moveHistory && testingOpening) {
+      testingMoves(moveHistory, testingOpening);
+    }
+  }, [moveHistory]);
+  function testingMoves(
+    moveHistory: moveHistory[],
+    testingOpening: moveHistory[]
+  ) {
+    for (let i = 0; i < moveHistory.length; i++) {
+      if (
+        moveHistory[i].newAnnotatedPosition ===
+        testingOpening[i].newAnnotatedPosition
+      ) {
+        console.log("CORRECT");
+      } else {
+        console.log("FALSE");
+      }
+    }
+    return;
+  }
   function saveOpening() {
     setTestingOpening(moveHistory);
     setMoveHistory([]);
     setResetBoard(true);
+    setTestingMode(true);
+    console.log(testingOpening);
   }
   return (
     <button
